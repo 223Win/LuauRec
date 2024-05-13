623eedde406515150c0270a895588829b204301e
lkm00 <lkm00@outlook.com>
2024-05-12 20:39:21 -0500

    SetList OpCode Converter

--- Luau/OpCodeConverter.lua
+++ Luau/OpCodeConverter.lua
@@ -14,7 +14,7 @@ local OpCodeData = {
     ---@param Refs table<number,string>
     ---@param Vals table<number,string>
     ["LOADK"] = function(Refs, Vals)
-        reftable[Refs[1]] = Vals
+        reftable[Refs[1]] = Vals[1]
         return concat("local ",Refs[2]," = ",Vals[1])
     end,
     ---@type function
@@ -29,7 +29,7 @@ local OpCodeData = {
     ---@param Vals table<number,string>
     ["NEWTABLE"] = function(Refs, Vals)
         local t = {}
-        for i = 0, Refs[3] do
+        for i = 1, Refs[3] do
             table.insert(t, "unknown value: settable not called yet.")
         end
         reftable[Refs[1]] = t
@@ -39,7 +39,16 @@ local OpCodeData = {
     ---@param Refs table<number,string>
     ---@param Vals table<number,string>
     ["SETLIST"] = function(Refs, Vals)
-        
+        --//-- Set pointer back by 1 to fix pointer invalidation --//--
+        local curpointer = Pointers:GetPointer(Refs[2]) - 1
+        local t = reftable[Refs[1]]
+        for i = 1, Refs[3] do
+            curpointer = curpointer + 1
+            t[i] = reftable[Pointers:SetPointer(curpointer)]
+        end
+        reftable[Refs[1]] = t
+        print(table.concat(reftable[Refs[1]],", \n"))
+        return "--SETLIST OPCODE SKIP"
     end,
 }
 
